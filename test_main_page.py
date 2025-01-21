30208c68794317cb549e2f02e2f4f7c05d4d059c
unknown <elshadmursalov8@gmail.com>
2025-01-21 23:24:10 +0300

    new

--- test_main_page.py
+++ test_main_page.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     browser.get(link)
-    page = MainPage(browser,link)
+    page = MainPage(browser,link)# инициализируем Page Object, передаем в конструктор экземпляр драйвера и link адрес
     page.open()
     page.go_to_login_page()
 
