2096975d460e468c3aacbef6c89da45b1f8266ac
unknown <elshadmursalov8@gmail.com>
2025-01-24 01:16:31 +0300

    realize LoginPage

--- pages/locators.py
+++ pages/locators.py
@@ -1,4 +1,14 @@
 from selenium.webdriver.common.by import By
 
 class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR," #login_link")
+    LOGIN_LINK = (By.CSS_SELECTOR,"#login_link")
+class LoginPageLocators():
+    INPUT_EMAIL = (By.CSS_SELECTOR,"#id_login-username")
+    INPUT_PASSWORD = (By.CSS_SELECTOR,"#id_login-password")
+    BUTTON_SUBMIT = (By.NAME,"login_submit")
+    FORGET_PASSWORD = (By.CSS_SELECTOR,"#login_form > p > a")
+class RegistrPageLocators():
+    INPUT_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    INPUT_PASSWORD = (By.CSS_SELECTOR, "id_registration-password1")
+    INPUT_REPEAT_PASSWORD = (By.CSS_SELECTOR, "id_registration-password2")
+    BUTTON_REGISTRATION_SUBMIT = (By.NAME, "registration_submit")
---
+++ pages/login_page.py
@@ -0,0 +1,26 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+from .locators import RegistrPageLocators
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert "login" in self.browser.link,"Отсутствует логин в поле url"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.INPUT_EMAIL),"Отсутствует инпут логина"
+        assert self.is_element_present(*LoginPageLocators.INPUT_PASSWORD),"Отсутствует инпут пароля"
+        assert self.is_element_present(*LoginPageLocators.BUTTON_SUBMIT),"Отсуствует кнопка войти"
+        assert self.is_element_present(*LoginPageLocators.FORGET_PASSWORD),"Отсутствует кнопка забыли пароль"
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*RegistrPageLocators.INPUT_EMAIL), "Отсутствует инпут логина"
+        assert self.is_element_present(*RegistrPageLocators.INPUT_PASSWORD), "Отсутствует инпут пароля"
+        assert self.is_element_present(*RegistrPageLocators.INPUT_REPEAT_PASSWORD),"Отсуствует инпут повторного ввода пароля"
+        assert self.is_element_present(*RegistrPageLocators.BUTTON_REGISTRATION_SUBMIT), "Отсуствует кнопка войти"
