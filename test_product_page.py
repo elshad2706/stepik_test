dc3247ef7808da4cb0ec0623dd378f2297f585df
unknown <elshadmursalov8@gmail.com>
2025-02-01 17:11:35 +0300

    Added xfail test

--- test_product_page.py
+++ test_product_page.py
@@ -7,10 +7,13 @@ import pytest
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
 urls = [f"{link}/?promo=offer{no}" for no in range(10)]
+bugged_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7"
+okey_link1 = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"
+okey_link2 = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8"
 
-@pytest.mark.parametrize('link', urls)
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/"
+# @pytest.mark.parametrize('link', urls)
+@pytest.mark.parametrize('link', [okey_link1, pytest.param(bugged_link, marks=pytest.mark.xfail), okey_link2])
+def test_guest_can_add_product_to_basket(link,browser):
     browser.get(link)
     product_page = ProductPage(browser, link)
     product_page.open()
