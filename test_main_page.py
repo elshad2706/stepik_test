f58705da0d8f4cc0c01ab9830e6ffee425728e3c
unknown <elshadmursalov8@gmail.com>
2025-02-19 23:00:21 +0300

    add markers for tests

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 import time
 
+import pytest
 from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
 from selenium import webdriver
@@ -8,34 +9,32 @@ from .pages.basket_page import BasketPage
 from .pages.product_page import ProductPage
 
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    page = MainPage(browser,link) #инициализируем Page Object, передаем в конструктор экземпляр драйвера и link адрес
-    page.open()
-    login_page = page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+@pytest.mark.login_quest
+class TestLoginFromMainPage:
+    def test_guest_can_go_to_login_page(self,browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        browser.get(link)
+        page = MainPage(browser,link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и link адрес
+        page.open()
+        login_page = page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
+    def test_guest_cant_see_product_in_basket_opened_from_main_page(self,browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        browser.get(link)
+        page = BasketPage(browser, link)
+        page.open()
+        page.quest_click_button_view_basket()
+        page.product_is_not_exist_in_basket()
+        page.text_about_basket_empty()
 
-def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    page = BasketPage(browser, link)
-    page.open()
-    page.quest_click_button_view_basket()
-    page.product_is_not_exist_in_basket()
-    page.text_about_basket_empty()
-
-
-def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/"
-    browser.get(link)
-    page = BasketPage(browser, link)
-    page.open()
-    page.quest_click_button_view_basket()
-    page.product_is_not_exist_in_basket()
-    page.text_about_basket_empty()
-
-
-
+    def test_guest_cant_see_product_in_basket_opened_from_product_page(self,
+                                                                       browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/"
+        browser.get(link)
+        page = BasketPage(browser, link)
+        page.open()
+        page.quest_click_button_view_basket()
+        page.product_is_not_exist_in_basket()
+        page.text_about_basket_empty()
