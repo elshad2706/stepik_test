7acab2faf3655bb43e79b06483515e9b9ee18a46
unknown <elshadmursalov8@gmail.com>
2025-03-06 00:43:01 +0300

    changes in variable names

--- test_product_page.py
+++ test_product_page.py
@@ -21,8 +21,8 @@ link_product_2 = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city
 def test_guest_can_add_product_to_basket(links, browser):
     product_page = ProductPage(browser, links)
     product_page.open()
-    product_page.get_item_before()
-    product_page.get_price_before()
+    product_page.get_name_product()
+    product_page.get_price_product()
     product_page.add_to_basket()
     product_page.solve_quiz_and_get_code()
     product_page.compare_item_and_price_after_add_basket()
@@ -70,7 +70,7 @@ class TestUserAddToBasketFromProductPage:
     def test_user_can_add_product_to_basket(self, browser):
         product_page = ProductPage(browser, link_product_1)
         product_page.open()
-        product_page.get_item_before()
-        product_page.get_price_before()
+        product_page.get_name_product()
+        product_page.get_price_product()
         product_page.add_to_basket()
         product_page.compare_item_and_price_after_add_basket()
